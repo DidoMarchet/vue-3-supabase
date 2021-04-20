cdf2cbc5cd33cfde37606fcf57776faefb6b8cbe
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-04-20 19:06:04 +0200

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'prettier',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+  ],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.js
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+module.exports = {
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'vue-3-supabase'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'vue'
+        }
+      }
+    }
+  },
+  plugins: [
+    vue()
+  ]
+}
