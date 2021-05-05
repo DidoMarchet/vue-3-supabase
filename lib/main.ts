21d42da18eb82c8a3287933efc632f7c6cab4450
Tyler Mercer <40773575+tylermercer@users.noreply.github.com>
2021-05-05 08:36:38 -0600

    Fix potential type error on InjectionKey

--- lib/main.ts
+++ lib/main.ts
@@ -1,11 +1,11 @@
-import { App, inject } from "vue";
+import { App, inject, InjectionKey } from "vue";
 import { Params } from "./@types/index";
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 export interface IVueSupabase {
   install(app: App, params: Params): void;
 }
 
-const supabaseInjectionKey = Symbol();
+const supabaseInjectionKey: InjectionKey<SupabaseClient> = Symbol();
 
 export const useSupabase = () => inject(supabaseInjectionKey);
 
